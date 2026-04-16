938aa4aeefed7f5857e1320a3993367349b00aa0
AlketaThacii <alketa.thaci@student.uni-pr.edu>
2026-04-16 21:22:26 +0200

    Pranimi dhe shfaqja e mesazheve nga klientet

--- serverside.js
+++ serverside.js
@@ -5,6 +5,15 @@ const server = dgram.createSocket('udp4');
 const PORT = 5000;
 const HOST = '0.0.0.0';
 
+
+
+server.on('message', (msg, rinfo) => {
+    const message = msg.toString();
+    const clientId = rinfo.address + ":" + rinfo.port;
+
+    console.log(`Mesazh nga ${clientId}: ${message}`);
+});
+
 server.bind(PORT, HOST, () => {
     console.log("Serveri eshte aktiv...");
 });
