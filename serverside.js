fbadee6b902cbcf7c92e0fe30d5da04e4ed437e6
AlketaThacii <alketa.thaci@student.uni-pr.edu>
2026-04-16 16:31:56 +0200

    Aktivizimi i serverit

--- serverside.js
+++ serverside.js
@@ -1,3 +1,10 @@
 const dgram = require('dgram');
 const fs = require('fs');
 const server = dgram.createSocket('udp4');
+
+const PORT = 5000;
+const HOST = '0.0.0.0';
+
+server.bind(PORT, HOST, () => {
+    console.log("Serveri eshte aktiv...");
+});
