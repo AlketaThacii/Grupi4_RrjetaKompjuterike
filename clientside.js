fbaaa56fa528f29072702cec686d870a3c5d1c63
almajashari <alma.jashari2@student.uni-pr.edu>
2026-04-18 16:23:28 +0200

    mesazhi nga terminali

--- clientside.js
+++ clientside.js
@@ -1,4 +1,12 @@
 const dgram = require('dgram');
 const client = dgram.createSocket('udp4');
+
 const SERVER_IP = '192.168.1.100';
 const PORT = 5000;
+
+const message = process.argv[2];
+
+if (!message) {
+    console.log("Shkruaj nje komande!");
+    process.exit();
+}
