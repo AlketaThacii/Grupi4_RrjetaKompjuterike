8e6be153c80cd57c0ae81480c6932570b91c8e89
almajashari <alma.jashari2@student.uni-pr.edu>
2026-04-18 20:39:33 +0200

    rishikim dhe permiresim i kodit

--- clientside.js
+++ clientside.js
@@ -6,16 +6,40 @@ const PORT = 5000;
 
 const message = process.argv[2];
 
-if (!message) {
+if (!message || message.trim() === "") {
     console.log("Shkruaj nje komande!");
     process.exit();
 }
 
+if (message === "help") {
+    console.log(`
+        Komdandat:
+        read1
+        read2
+        write<tekst>
+        execute
+        list
+        admin1234
+        `);
+           process.exit();
+    }
+
+console.log(`Duke u lidhur me serverin ${SERVER_IP}:${PORT}`);
+console.log("Derguar:", message);
+
 client.send(message, PORT, SERVER_IP, (err) => {
-    if (err) throw err;
+    if (err) {
+        console.log("Gabim gjate dergimit:", err.message);
+        client.close();
+    }
 });
 
 client.on('message', (msg) => {
     console.log("Pergjigje:", msg.toString());
+    client.close();
+});
+
+client.on('error', (err)=> {
+    console.log("Gabim ne klient:", err.message);
     client.close();
 });
