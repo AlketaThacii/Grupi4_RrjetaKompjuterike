ce52853a2c87adf5039193476886019206bce11c
AlketaThacii <alketa.thaci@student.uni-pr.edu>
2026-04-18 19:55:57 +0200

    Numerimi i klienteve ne server

--- serverside.js
+++ serverside.js
@@ -4,6 +4,8 @@ const { handleRequest } = require('./logic');
 
 const server = dgram.createSocket('udp4');
 
+let activeClients = new Set();
+
 
 const PORT = 5000;
 const HOST = '0.0.0.0';
@@ -14,6 +16,9 @@ server.on('message', (msg, rinfo) => {
     const message = msg.toString();
     const clientId = rinfo.address + ":" + rinfo.port;
 
+    activeClients.add(rinfo.address);
+    console.log(`Klientë aktiv: ${activeClients.size}`);
+    
     console.log(`Mesazh nga ${clientId}: ${message}`);
 
     const response = handleRequest(message, clientId);
